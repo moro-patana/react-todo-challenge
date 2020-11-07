329a92fb4edb643e44ed3094d7f5b1f59bd507c0
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 15:26:10 +0300

    used switch and route

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from "react"
+import Header from "./components/Header"
 import FormInput from "./components/FormInput"
+import All from "./components/All"
+import {Switch, Route, Link} from "react-router-dom"
+console.log(All);
+
 function App() {
     return (
-      <main>
-          <h1>To do</h1>
-          <FormInput/>
-      </main>
+        <main>
+            <Header/>
+            <nav>
+                <ul>
+                    <Link to="/">All</Link>
+                    <Link to="/active">Active</Link>
+                    <Link to="/completed">Completed</Link>
+                </ul>
+            </nav>
+
+            <FormInput/>
+            <Switch>
+                <Route exact path="/">
+                  <All/>
+                </Route>
+                <Route path="/active">
+                  <All/>
+                </Route>
+                <Route path="/completed">
+                  <All/>
+                </Route>
+
+            </Switch>
+        </main>
     )
 }
 export default App;
