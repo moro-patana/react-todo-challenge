2b7f2c8381291e48ed8c328bff294e355cf38af9
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 09:37:44 +0300

    stored the compenents files in this folder

---
+++ components/FormInput.js
@@ -0,0 +1,16 @@
+import React from "react"
+function FormInput() {
+    return (
+        <di>
+            <form>
+                <input
+                  type="text"
+                  value="text"
+                  onChange=""
+                />
+                <button>Add</button>
+            </form>
+        </di>
+    )
+}
+export default FormInput;
