0e18e84dc8d9d1e1b8798935b7377d9685a4c80c
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 15:30:30 +0300

    set the state

---
+++ components/useToggler.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+function UseToggler() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [isChecked, setIsChecked] = useState(false)
+    function handleChecked() {
+        setIsChecked(!isChecked)
+    }
+  
+    const handleChange = e => {
+      setTodo(e.target.value);
+    };
+    const addTodo = () => {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo,
+          completed: false
+        }
+      ]);
+    };
+  
+    const onSubmit = e => {
+      e.preventDefault();
+      if (todo === "") return;
+      addTodo();
+      setTodo("");
+    };
+  
+    const removeTodo = todoId => {
+      const updatedTodos = todos.filter(todo => todo.id !== todoId);
+      setTodos(updatedTodos);
+    };
+  
+    const toggleTodo = todoId => {
+      const updatedTodos = todos.map(todo => {
+        return todo.id === todoId
+          ? { ...todo, completed: !todo.completed }
+          : todo;
+      });
+      setTodos(updatedTodos);
+    };
+  
+return({todo, todos, isChecked, handleChecked, handleChange, addTodo, onSubmit, toggleTodo})  
+}
+export default UseToggler;
