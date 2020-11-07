3a38bba1aa6f511d60e60cc31aa6c1b86c4c35d9
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 09:38:47 +0300

    imported Router, wrapped the App function inside the Router

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import FormInput from "./components/FormInput"
+function App() {
+    return (
+      <main>
+          <h1>To do</h1>
+          <FormInput/>
+      </main>
+    )
+}
+export default App;
