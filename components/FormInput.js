eebe3a8942a179529a381f2972e3e111b73dfad2
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 16:24:43 +0300

    remove the onChange and value in the input

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,40 +1,43 @@
 import React from "react"
 import UseToggler from "./useToggler"
+import All from "./All";
+
 function FormInput() {
     const {todo, todos, isChecked, handleChecked, handleChange, addTodo, onSubmit, toggleTodo} = UseToggler()
+    console.log(todos);
+    
  return (
     <div className="container">
       <form onSubmit={onSubmit}>
         <input
           id="todo"
           className="todo-input"
-          onChange={handleChange}
-          value={todo}
+          name="todo"
+        //   onChange={handleChange}
+        //   value={todo}
         />
         <button type="submit" className="add-btn">
           Add
         </button>
       </form>
       <div>
-              {todos.map(todo => (
-                <fieldset>
-                    <input type="checkbox" key={todo.id} onChange={handleChecked}/>
-                    {isChecked
-                      ? <s><label>{todo.text}</label></s>
-                      :<label
-                        // className={todo.completed ? "todo-completed" : undefined}
-                        // onClick={() => toggleTodo(todo.id)}
-                      >
-                        {todo.text}
-                      </label>
-                    
-                    }
-                      {/* <span className="delete-btn" onClick={() => removeTodo(todo.id)}>
-                        x
-                      </span> */}
-                </fieldset>
-              ))}
-      </div>
+            <h1>Hello</h1>
+            {todos.map(todo => (
+            <fieldset>
+                <input type="checkbox" key={todo.id}/>
+                    <label
+                    // className={todo.completed ? "todo-completed" : undefined}
+                    // onClick={() => toggleTodo(todo.id)}
+                    >
+                    {todo.text}
+                    </label>
+                    {/* <span className="delete-btn" onClick={() => removeTodo(todo.id)}>
+                    x
+                    </span> */}
+            </fieldset>
+            ))}
+    </div>
+
     </div>
   );
 }
