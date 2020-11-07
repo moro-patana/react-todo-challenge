ef6fc6c6f457879bb99f8e43a36523158f59b3ea
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-07 15:27:23 +0300

    put the form element in this file

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,16 +1,41 @@
 import React from "react"
+import UseToggler from "./useToggler"
 function FormInput() {
-    return (
-        <di>
-            <form>
-                <input
-                  type="text"
-                  value="text"
-                  onChange=""
-                />
-                <button>Add</button>
-            </form>
-        </di>
-    )
+    const {todo, todos, isChecked, handleChecked, handleChange, addTodo, onSubmit, toggleTodo} = UseToggler()
+ return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <input
+          id="todo"
+          className="todo-input"
+          onChange={handleChange}
+          value={todo}
+        />
+        <button type="submit" className="add-btn">
+          Add
+        </button>
+      </form>
+      <div>
+              {todos.map(todo => (
+                <fieldset>
+                    <input type="checkbox" key={todo.id} onChange={handleChecked}/>
+                    {isChecked
+                      ? <s><label>{todo.text}</label></s>
+                      :<label
+                        // className={todo.completed ? "todo-completed" : undefined}
+                        // onClick={() => toggleTodo(todo.id)}
+                      >
+                        {todo.text}
+                      </label>
+                    
+                    }
+                      {/* <span className="delete-btn" onClick={() => removeTodo(todo.id)}>
+                        x
+                      </span> */}
+                </fieldset>
+              ))}
+      </div>
+    </div>
+  );
 }
 export default FormInput;
